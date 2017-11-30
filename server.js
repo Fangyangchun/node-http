7f2f5132dc45cb8baf785ff774c2a91d2105dfad
Fangyangchun <1697095271@qq.com>
2017-11-30 14:50:53 +0800

    判定请求方式

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
+// var http = require('http');
+// var server = new http.Server();
+
+// server.on('request', function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write('<h1>Node.js</h1>');
+//     res.end('<p>HelloWorld</p>');
+// });
+
+// server.listen(3000);
+// console.log('HTTP SERVER is LISTENING AT PORT 3000.');
+
 var http = require('http');
-var server = new http.Server();
+var querystring = require('querystring');
+var util = require('util');
 
-server.on('request', function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>HelloWorld</p>');
-});
+// var iconv = require('iconv-lite'); 
+// var BufferHelper = require('bufferhelper');
 
-server.listen(3000);
-console.log('HTTP SERVER is LISTENING AT PORT 3000.');
+http.createServer(function(req, res) {
+    const method = req.method;
+    if (method === 'POST') {
+        var post = '';
+        req.on('data', function(chunk) {
+            post += chunk;
+        });
+        req.on('end', function() {
+            post = querystring.parse(post);
+            res.end(util.inspect(post));
+        })
+    }
+    if (method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Node.js</h1>');
+        res.end('<p>HelloWorld</p>');
+    }
+    
+}).listen(3000);
+console.log('localhost:3000');
